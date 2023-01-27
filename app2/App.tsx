17f66f442b8a8003f1d7c7dac598e2c68d735157
kavinduUdhara <88233364+kavinduUdhara@users.noreply.github.com>
2023-01-27 16:21:08 +0000

    almost done

--- app2/App.tsx
+++ app2/App.tsx
@@ -1,15 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { SafeAreaView, Text, Button, StyleSheet, Pressable} from "react-native";
+import { Alert } from 'react-native';
 
 export default function App() {
+  const [data1, data2] = useState('data1');
+
   const ui = (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.text}>{data1}</Text>
+      <Button title="change" onPress={() => chage_the_text1('sucess')}/>
+    </SafeAreaView>
   );
   return ui;
+
+  function chage_the_text1(text: string){
+    data2('data2');
+  }
 }
+
 
 const styles = StyleSheet.create({
   container: {
@@ -18,4 +26,17 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  text: {
+    margin : 20
+  },
+
+  button: {
+    margin: 20,
+    padding: 20
+  }
 });
+
+function chage_the_text(text: string){
+  Alert.alert('message',text);
+}
