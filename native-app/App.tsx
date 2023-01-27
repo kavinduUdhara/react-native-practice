bb2dc5f7bf9b797db1ef0a1f7f16cc7965934a1d
kavinduUdhara <88233364+kavinduUdhara@users.noreply.github.com>
2023-01-27 14:19:38 +0000

    created simple view app

--- native-app/App.tsx
+++ native-app/App.tsx
@@ -1,22 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
+import React from "react";
+import { SafeAreaView, Text, Button, StyleSheet} from "react-native";
 
-import useCachedResources from './hooks/useCachedResources';
-import useColorScheme from './hooks/useColorScheme';
-import Navigation from './navigation';
+function app(){
+  const ui = (
+    <SafeAreaView style=(styles.mian)>
+      <Text>Hello world</Text>
+      <Button title="change"/>
+    </SafeAreaView>
+  );
 
-export default function App() {
-  const isLoadingComplete = useCachedResources();
-  const colorScheme = useColorScheme();
-
-  if (!isLoadingComplete) {
-    return null;
-  } else {
-    return (
-      <SafeAreaProvider>
-        <Navigation colorScheme={colorScheme} />
-        <StatusBar />
-      </SafeAreaProvider>
-    );
-  }
+  return ui;
 }
+
+export default app;
+
+const styles = StyleSheet.create(
+  {
+    main:{
+      flex: 1,
+      justifyContent: 'center',
+      alignItems : "center"
+    },
+    text:{
+      fontSize : 24,
+      fontWeight : "bold"
+    }
+  }
+);
