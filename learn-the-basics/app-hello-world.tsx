5662bfefb4d01fe39ac304fc3a7ebe84c101b4a1
kavinduUdhara <88233364+kavinduUdhara@users.noreply.github.com>
2023-02-04 22:36:58 +0530

    Update app-hello-world.tsx

--- learn-the-basics/app-hello-world.tsx
+++ learn-the-basics/app-hello-world.tsx
@@ -1,17 +1,19 @@
 import React from "react";
 import { Text, View } from "react-native";
 
-const helloWorldApp  = () => {
+// HelloWorldApp component that returns a View component with centered Text component
+const HelloWorldApp = () => {
   return (
     <View
+      // Style object to position the View component in the center of the screen
       style={{
-        flex : 1,
-        justifyContent : 'center',
-        alignItems : 'center'
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
       }}>
-        <Text>Hello, world!</Text>
+      <Text>Hello, world!</Text>
     </View>
   );
 };
 
-export default helloWorldApp;
+export default HelloWorldApp;
