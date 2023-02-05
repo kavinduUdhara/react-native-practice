237e029ebf437afda1413c5f83d1974628a55d9f
kavinduUdhara <88233364+kavinduUdhara@users.noreply.github.com>
2023-02-05 09:29:51 +0000

    done: Call Footer Component from App Component

--- coursera-course-meta/components/Footer.tsx
+++ coursera-course-meta/components/Footer.tsx
@@ -3,7 +3,7 @@ import { View, Text } from "react-native";
 
 export default function footer() {
     return(
-        <View style={{flex: 1, justifyContent: "flex-end" }}>
+        <View style={{flex: 1, justifyContent: "flex-end", backgroundColor: 'white'}}>
             <Text style={{ backgroundColor: '#F4CE14', padding: 5,textAlign: 'center' }}>All rights reserved by Little Lemon, 2022</Text>
         </View>
     );
