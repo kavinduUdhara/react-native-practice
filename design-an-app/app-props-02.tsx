2e7ffda9bb1c5d5f2b6d3cc54f939cf6d257397c
kavinduUdhara <88233364+kavinduUdhara@users.noreply.github.com>
2023-02-08 07:00:27 +0000

    copy learn-the-basics file

---
+++ design-an-app/app-props-02.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+    center : {
+        alignItems : 'center',
+    }
+});
+
