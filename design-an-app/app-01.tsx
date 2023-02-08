7056d6979f4f7f99b9058203732a443b0acc9e78
kavinduUdhara <88233364+kavinduUdhara@users.noreply.github.com>
2023-02-08 07:13:44 +0000

    copied learn-the-basic file as

---
+++ design-an-app/app-01.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Text, View, StyleSheet } from "react-native";
+
+const App = () => {
+    return(
+        <View>
+            <Text>
+                Hello world
+            </Text>
+        </View>
+    );
+};
+
+export default App;
--- design-an-app/app-props-02.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { View, Text, StyleSheet } from "react-native";
-
-const styles = StyleSheet.create({
-    center : {
-        alignItems : 'center',
-    }
-});
-
